1901fdddb4682b6f738fa8a7faef5ea77ab92007
Ray C Horn <raychorn@gmail.com>
2021-01-18 19:36:42 +0000

    updates. fixing the deployment system.

--- update-build.sh
+++ update-build.sh
@@ -3,15 +3,7 @@
 LIB_SRC=/workspaces/private-microservices-framework/microservices_framework/python_lib3/private_vyperlogix_lib3/zips
 FRAMEWORK_SRC=/workspaces/private-microservices-framework/microservices_framework
 LIB_DEST=/workspaces/microservices-framework/microservices-framework/python_lib3
-FRAMEWORK_DEST=/workspaces/microservices-framework/microservices-framework/microservices_framework
-
-
-if [[ -d $LIB_SRC ]]
-then
-    echo "Updating ZIPS"
-    rm -f $LIB_DEST/*.zip
-    cp $LIB_SRC/*.zip $LIB_DEST
-fi
+FRAMEWORK_DEST=/workspaces/microservices-framework/microservices-framework
 
 if [[ -d $FRAMEWORK_SRC ]]
 then
@@ -22,3 +14,11 @@ then
     rm -R -f $FRAMEWORK_DEST/python_lib3/*
     find $FRAMEWORK_DEST -name __pycache__ -exec rm -rf {} \;
 fi
+
+if [[ -d $LIB_SRC ]]
+then
+    echo "Updating ZIPS"
+    rm -f $LIB_DEST/*.zip
+    cp $LIB_SRC/*.zip $LIB_DEST
+fi
+
