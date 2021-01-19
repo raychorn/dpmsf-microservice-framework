c01b9dc11e30d216599a387c6ad1f989039a0263
Ray C Horn <raychorn@gmail.com>
2021-01-19 01:16:51 +0000

    updates. added docker-compose for deployment.

---
+++ docker/docker-logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ARRAY=()
+ARRAY+=("mongodb")
+ARRAY+=("vyperapi")
+
+PS3="Choose: "
+
+select option in "${ARRAY[@]}";
+do
+    echo "Choose: $REPLY"
+    choice=${ARRAY[$REPLY-1]}
+    break
+done
+
+CID=$(docker ps -qf "name=$choice")
+echo "CID=$CID"
+if [[ ! $CID. == . ]]
+then
+    echo "Showling logs $CID"
+    docker logs --tail 2500 $CID
+fi
