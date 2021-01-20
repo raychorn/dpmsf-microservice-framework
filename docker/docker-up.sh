aa583fd3f6d289636f8845b36b9674e2324ea4b8
Ray C Horn <raychorn@gmail.com>
2021-01-20 21:22:28 +0000

    updates.  getting closer to functional production.

--- docker/docker-up.sh
+++ docker/docker-up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+vyperapi="vyperapi"
+
 mkdir -p ./mongodb/data/log/
 #touch ./mongodb/data/log/mongod.log
 sudo chown -R root:root ./mongodb
@@ -8,3 +10,12 @@ mkdir -p ./django/logs/
 sudo chown -R root:root ./django
 sudo chmod -R 0777 ./django
 docker-compose up -d
+sleep 15s
+CID=$(docker ps -qf "name=$vyperapi")
+echo "CID=$CID"
+if [[ ! $CID. == . ]]
+then
+    echo "Restarting $CID"
+    docker restart $CID
+fi
+service nginx restart
