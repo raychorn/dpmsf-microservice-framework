08fbe88c46de62a308f7cb65b16ee9766391a0b5
Ray C Horn <raychorn@gmail.com>
2021-01-20 00:13:43 +0000

    updates. docker.

--- docker/docker-up.sh
+++ docker/docker-up.sh
@@ -2,6 +2,9 @@
 
 mkdir -p ./mongodb/data/log/
 #touch ./mongodb/data/log/mongod.log
-#chown -R root:root ./mongodb
+sudo chown -R root:root ./mongodb
+sudo chmod -R 0777 ./mongodb
 mkdir -p ./django/logs/
+sudo chown -R root:root ./django
+sudo chmod -R 0777 ./django
 docker-compose up -d
