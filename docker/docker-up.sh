dbb768026faaeacf5c9a2d4284577394dea6ba31
Ray C Horn <raychorn@gmail.com>
2021-01-24 19:27:58 +0000

    updates. updated the docker scripts for the latest release. 3.0.2.

--- docker/docker-up.sh
+++ docker/docker-up.sh
@@ -9,13 +9,31 @@ sudo chmod -R 0777 ./mongodb
 mkdir -p ./django/logs/
 sudo chown -R root:root ./django
 sudo chmod -R 0777 ./django
+
+ARRAY=()
+ARRAY+=('/workspaces/admin-plugins')
+
+for workspace in "${ARRAY[*]}"
+do
+    echo "workspace  : $workspace"
+    if [[ ! -d $workspace ]]
+    then
+        echo "Created workspace: $workspace"
+        mkdir -p $workspace
+    fi
+done
+
 docker-compose up -d
 sleep 15s
+
 CID=$(docker ps -qf "name=$vyperapi")
 echo "CID=$CID"
 if [[ ! $CID. == . ]]
 then
+    echo "vyperapi is running"
     echo "Restarting $CID"
     docker restart $CID
 fi
+sleep 5s
 sudo service nginx restart
+sudo service nginx status
