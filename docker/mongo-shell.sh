f3d4c99e1c6213de822f4dc3997c37208040c071
Ray C Horn <raychorn@gmail.com>
2021-01-25 21:52:34 +0000

    updates. fixed the mongo-shell.sh script.

--- docker/mongo-shell.sh
+++ docker/mongo-shell.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ ! -f .env ]
-then
-    export $(cat .env | xargs)
-fi
-
-mongo --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD  --authenticationDatabase $MONGO_INITDB_DATABASE --host 10.5.0.5 --port 27017
+mongo --username root --password --authenticationDatabase $MONGO_INITDB_DATABASE --host 10.5.0.5 --port 27017
